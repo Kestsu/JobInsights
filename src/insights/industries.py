c675a3e9f8867a63f95b8c7f8d59f7400f9cccbf
Kevin Li <Kevinstart14@gmail.com>
2023-01-24 19:51:57 -0300

    feito 7

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -13,18 +13,9 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
+    lista = []
+    for item in jobs:
+        if industry == item["industry"]:
+            lista.append(item)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return lista
