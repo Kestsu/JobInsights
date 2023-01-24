cd5ae2392c6ee585b51f1a8a8aaaf9d819986fd4
Kevin Li <Kevinstart14@gmail.com>
2023-01-24 19:48:04 -0300

    feito 6

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -19,18 +19,9 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
+    lista = []
+    for item in jobs:
+        if job_type == item["job_type"]:
+            lista.append(item)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    return lista
