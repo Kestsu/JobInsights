97cbd2019935f0c6d5655c09414fe7f6a19e4dca
Kevin Li <Kevinstart14@gmail.com>
2023-01-24 22:14:26 -0300

    feito testes pro count_word_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,4 +1,5 @@
 from src.pre_built.counter import count_ocurrences
+from tests.counter.mocks import count_word_ocurrences
 
 
 def test_counter():
@@ -6,3 +7,12 @@ def test_counter():
     result2 = count_ocurrences("data/jobs.csv", "JAVASCRIPT")
     assert result1 == 122
     assert result2 == 122
+    assert result1 == result2
+
+    result3 = count_word_ocurrences("data/jobs.csv", "Javascript")
+    result4 = count_word_ocurrences("data/jobs.csv", "JAVASCRIPT")
+    assert result3 == 32
+    assert result4 == 0
+
+    assert result1 != result3
+    assert result3 != result4
