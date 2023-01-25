46f45744102a118cf4d7b5b849e9c78533bc8b3f
Kevin Li <Kevinstart14@gmail.com>
2023-01-24 22:01:56 -0300

    test 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -4,6 +4,6 @@ from src.pre_built.counter import count_ocurrences
 def test_counter():
     result1 = count_ocurrences("data/jobs.csv", "Javascript")
     result2 = count_ocurrences("data/jobs.csv", "JAVASCRIPT")
-    assert result1 == 32
-    assert result2 == 0
-    print(result2)
+    assert result1 == 122
+    assert result2 == 122
+
