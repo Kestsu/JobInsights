0af8e9aaa83679d62845d0b80cb67f72a79fc9b2
Kevin Li <Kevinstart14@gmail.com>
2023-01-24 21:10:21 -0300

    feito 9

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -50,18 +50,24 @@ def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
+    lista = []
+    for item in jobs:
+        try:
+            if matches_salary_range(item, salary):
+                lista.append(item)
+        except (ValueError):
+            pass
+    return lista
 
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+
+# if __name__ == "__main__":
+#     b = [
+#         {"max_salary": 0, "min_salary": 10},
+#         {"max_salary": 10, "min_salary": 100},
+#         {"max_salary": 10000, "min_salary": 200},
+#         {"max_salary": 15000, "min_salary": 0},
+#         {"max_salary": 1500, "min_salary": 0},
+#         {"max_salary": -1, "min_salary": 10},
+#     ]
+#     print(filter_by_salary_range(b, 100))
